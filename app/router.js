c1624116394b5b650c1f4086a6c50175e07ef4bb
Jorrit Meulenbeld <jorrit.meulenbeld@fabriquartz.com>
2015-10-14 09:27:35 +0200

    first blood

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('bands', {});
+  this.route('songs', {});
 });
 
 export default Router;
---
+++ app/routes/songs.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
+
+var blackDog = Song.create({
+  title: 'Black Dog',
+  band: 'Led Zeppelin',
+  rating: 3
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+var SongCollection = Ember.Object.extend({
+  content: [],
+  sortProperties: ['rating:desc'],
+  sortedContent: Ember.computed.sort('content', 'sortProperties'),
+});
+
+var songs = SongCollection.create();
+songs.get('content').pushObjects([blackDog, yellowLedbetter, pretender]);
+
+export default Ember.Route.extend({
+  model: function() {
+    return songs.get('sortedContent');
+  }
+});
