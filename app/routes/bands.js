8e5478cb6d328eed555b77ebd35bd8cd5a5d5b82
Jorrit Meulenbeld <jorrit.meulenbeld@fabriquartz.com>
2015-11-20 00:49:20 +0100

    added network delay simulation

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,8 +1,20 @@
 import Ember from 'ember';
 
+// simulate network delay
+function wait(promise, delay) {
+  return new Ember.RSVP.Promise(function(resolve) {
+    setTimeout(function() {
+      promise.then(function(result) {
+        resolve(result);
+      });
+    }, delay);
+  });
+}
+
 export default Ember.Route.extend({
   model: function() {
-    return this.store.findAll('band');
+    var bands = this.store.findAll('band');
+    return wait(bands, 3*1000);
   },
 
   actions: {
